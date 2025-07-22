7555d1c2d6ee055fe55f474a921aef65c433ee8e
Prabhagaran2004 <prabhagaran158@gmail.com>
2025-07-22 20:19:51 +0530

    Project completed

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,10 @@ const PORT = 3000 || process.env.PORT
 const connectDB = require('./lib/db');
 const cookieParser = require('cookie-parser');
 const { app , server } = require('./lib/socket');
+const path = require('path')
+
+
+const __dirname = path.resolve()
 
 app.use(cookieParser());
 app.use(cors({
@@ -18,6 +22,14 @@ app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use('/api/auth' , authRoutes)
 app.use('/api/messages' , messageRoutes) 
+
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
+}
 
 server.listen(PORT , ()=>{
     console.log(`Server is running on port ${PORT}`);
