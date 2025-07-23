ddc1e218d33cfeecb7b1b8f843a65f2dc1a0ee6d
Prabhagaran2004 <prabhagaran158@gmail.com>
2025-07-23 16:10:40 +0530

    module added

--- backend/index.js
+++ backend/index.js
@@ -3,12 +3,13 @@ const authRoutes = require('./routes/auth.route');
 const messageRoutes = require('./routes/message.route');
 const cors = require('cors');
 const dotenv = require('dotenv');
-const PORT = 3000 || process.env.PORT
+const PORT = process.env.PORT || 3000;
 const connectDB = require('./lib/db');
 const cookieParser = require('cookie-parser');
 const { app , server } = require('./lib/socket');
 const path = require('path')
 
+dotenv.config();
 
 
 app.use(cookieParser());
@@ -16,7 +17,6 @@ app.use(cors({
     origin: 'http://localhost:5173', 
     credentials: true, 
 }))
-dotenv.config();
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ limit: '10mb', extended: true }));
 app.use('/api/auth' , authRoutes)
